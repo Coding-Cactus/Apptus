a68256d006c8bed3c367b547ec1990220d95883f
Coding-Cactus <codingcactus.cc@gmail.com>
2023-03-29 17:48:11 +0100

    :passport_control: Add basic auth flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { in: 2..255 }
-  validates :colour, presence: true, format: /#[A-F0-9]{6}/
+  validates :colour, allow_blank: true, format: /#[A-F0-9]{6}/
+
+  before_create do
+    self.colour = %w[#3D9DD2 #6046FC #74CD55 #D69637 #2CAEA6 #DB43C3 #EF3E3E #37BC74].sample
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'chats#index'
 
-  devise_for :users
+  devise_for :users, skip: %i[sessions registrations passwords]
+
+  devise_scope :user do
+    get    'login',  to: 'devise/sessions#new',     as: :new_user_session
+    post   'login',  to: 'devise/sessions#create',  as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    put    'account',        to: 'devise/registrations#update'
+    delete 'account',        to: 'devise/registrations#destroy'
+    post   'account',        to: 'devise/registrations#create'
+    get    'register',       to: 'devise/registrations#new',    as: :new_user_registration
+    get    'account',        to: 'devise/registrations#edit',   as: :edit_user_registration
+    patch  'account',        to: 'devise/registrations#update', as: :user_registration
+    get    'account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+
+    put   'set-password',   to: 'devise/passwords#update'
+    post  'set-password',   to: 'devise/passwords#create'
+    patch 'set-password',   to: 'devise/passwords#update', as: :user_password
+    get   'set-password',   to: 'devise/passwords#edit',   as: :edit_user_password
+    get   'reset-password', to: 'devise/passwords#new',    as: :new_user_password
+  end
 end
