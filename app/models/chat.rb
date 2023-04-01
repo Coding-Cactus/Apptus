cbe2615e614de0d76b36aa2b48f8798052fc39ea
Coding-Cactus <codingcactus.cc@gmail.com>
2023-04-01 19:33:31 +0100

    :bug: Fix chat name length validation

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,7 +3,7 @@ class Chat < ApplicationRecord
   has_many :chat_members
   has_many :users, through: :chat_members
 
-  validates :name, presence: true, length: { between: 1..30 }
+  validates :name, presence: true, length: { in: 1..30 }
   validates :colour, allow_blank: true, format: /#[A-F0-9]{6}/
 
   before_create do
