e3d3add2a3f9a752f60d1d7412c64ee025fe085f
Coding-Cactus <codingcactus.cc@gmail.com>
2023-04-08 17:08:12 +0100

    :globe_with_meridians: Convert displayed times to local time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def nice_time(time)
-    now = Time.now
-
-    if time.day == now.day && time.month == now.month && time.year == now.year
-      time.strftime('%k:%M')
-    elsif time.year == now.year
-      time.strftime('%d/%m')
-    else
-      time.strftime('%d/%m/%y')
-    end
-  end
 end
