ccf8c6bd9bdea73dd8f2ccdc54b504b5f013c745
Coding-Cactus <codingcactus.cc@gmail.com>
2023-07-25 18:04:53 +0100

    :white_check_mark: Add user model tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,6 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include ActionMailer::TestHelper
+  include Rails.application.routes.url_helpers
 end
