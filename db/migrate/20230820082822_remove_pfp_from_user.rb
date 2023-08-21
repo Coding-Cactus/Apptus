13bbbe4bdedbdbcccf55c62de12b7aebb8b613b5
Coding-Cactus <codingcactus.cc@gmail.com>
2023-08-21 15:12:58 +0100

    :art: Rubocop

--- db/migrate/20230820082822_remove_pfp_from_user.rb
+++ db/migrate/20230820082822_remove_pfp_from_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemovePfpFromUser < ActiveRecord::Migration[7.0]
   def up
     remove_column :users, :pfp
--- db/migrate/20230820083608_remove_pfp_from_chat.rb
+++ db/migrate/20230820083608_remove_pfp_from_chat.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemovePfpFromChat < ActiveRecord::Migration[7.0]
   def up
     remove_column :chats, :pfp
