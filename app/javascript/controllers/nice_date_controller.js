b5c419a8ecdbbe32031a2c1f8bdab50e3ac0bd58
Coding-Cactus <codingcactus.cc@gmail.com>
2023-08-22 14:13:31 +0100

    :children_crossing: Show time with date for 24hr+ old messages

--- app/javascript/controllers/nice_date_controller.js
+++ app/javascript/controllers/nice_date_controller.js
@@ -5,16 +5,25 @@ export default class extends Controller {
         const now = new Date()
         const date = new Date(Number(this.element.getAttribute("data-timestamp")) * 1000)
 
+        let text = ""
+        const parent = this.element.parentNode
+        if (parent.classList.contains("msg-info") || parent.classList.contains("system-message")) {
+            text = date.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" }) + " "
+        }
+
         if (
             date.getDate()     === now.getDate()  &&
             date.getMonth()    === now.getMonth() &&
             date.getFullYear() === now.getFullYear()
         ) {
+            // Today
             this.element.innerText = date.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" })
         } else if (date.getFullYear() === now.getFullYear()) {
-            this.element.innerText = date.toLocaleDateString(undefined, { day: "2-digit", month: "2-digit" })
+            // This Year
+            this.element.innerText = text + date.toLocaleDateString(undefined, { day: "2-digit", month: "2-digit" })
         } else {
-            this.element.innerText = date.toLocaleDateString(undefined, { day: "2-digit", month: "2-digit", year: "2-digit" })
+            // Over a year ago
+            this.element.innerText = text + date.toLocaleDateString(undefined, { day: "2-digit", month: "2-digit", year: "2-digit" })
         }
 
         if (this.element.innerText === "Invalid Date") this.element.innerText = ""
